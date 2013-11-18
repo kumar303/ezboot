932b40230005de1d850df80e5509f964425cfbbf
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-11-18 14:03:53 -0600

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def local_file(fn):
 
 
 setup(name='ezboot1',
-      version='1.1.9',
+      version='1.1.10',
       description="Automatically configure a Boot2Gecko Device. It's so ez!",
       long_description=local_file('README.rst').read(),
       author='Kumar McMillan',
