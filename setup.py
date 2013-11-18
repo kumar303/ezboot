d7689672e939c2254e677d008e8ecc2d2687665f
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-11-18 10:50:52 -0600

    Bump version after 1.0 branch cut

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def local_file(fn):
 
 
 setup(name='ezboot',
-      version='1.1.8',
+      version='2.0.0',
       description="Automatically configure a Boot2Gecko Device. It's so ez!",
       long_description=local_file('README.rst').read(),
       author='Kumar McMillan',
